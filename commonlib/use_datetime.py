09e47553443a05796a6298edafc539fb287a1739
fxyfdf <fxyfdf@sina.com>
2018-11-26 22:17:54 +0800

    Default Changelist

--- commonlib/use_datetime.py
+++ commonlib/use_datetime.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+
+#获取当前时间
+now = datetime.now()
+print ("now=",now)
+print ("type(now)=",type(now))
+
+#用指定日期时间创建 datetime
+dt = datetime(2015,4,19,12,20)
+print ("dt=",dt)
