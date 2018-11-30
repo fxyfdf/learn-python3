7efb6f727fe1e3a6502d92533cbdad80fc1ad34c
fxyfdf@sina.com <fxyfdf@sina.com>
2018-11-30 18:30:09 +0800

    scrapy 2018.11.30

--- commonlib/use_itertools.py
+++ commonlib/use_itertools.py
@@ -25,13 +25,17 @@ Python的内建模块itertools提供了非常有用的用于操作迭代对象�
 #######################################################################
 """
 takewhile 
+无限序列只有在for迭代时才会无限地迭代下去，如果只是创建了一个迭代对象，它不会事先把无限个元素生成出来，事实上也不可能在内存中创建无限多个元素。
+无限序列虽然可以无限迭代下去，但是通常我们会通过takewhile()等函数根据条件判断来截取出一个有限的序列：
 """
 natuals = itertools.count(1)
 print (natuals)
 ns = itertools.takewhile(lambda x: x <= 10, natuals)
 print (list(ns))
 
-
+#chain()可以把一组迭代对象串联起来，形成一个更大的迭代器：
+for c in itertools.chain('ABC', 'XYZ'):
+    print (c)
 
 
 
